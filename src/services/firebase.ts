546808c7313ed00f3de16cead0f157a78b0cdb3b
olexpresske <denniskmuthoni@gmail.com>
2026-09-17 00:28:50 +0300

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,13 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-const firebaseConfig = {
-  apiKey: "AIzaSyDc6N5dULG6i72EWrlTtunwhbKQxaVi2KY",
-  authDomain: "olexpresske.firebaseapp.com",
-  databaseURL: "https://olexpresske-default-rtdb.firebaseio.com",
-  projectId: "olexpresske",
-  storageBucket: "olexpresske.firebasestorage.app",
-  messagingSenderId: "953335654623",
-  appId: "1:953335654623:web:1b91cbd29c1f63a23ba16a"
-};
-const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+
